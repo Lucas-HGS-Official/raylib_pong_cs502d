7e97c4a365f455a9cf8850efa77315962c762cf2
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-06-11 11:09:42 -0300

    Paddle update

--- code/include/paddle.h
+++ code/include/paddle.h
@@ -4,16 +4,20 @@
 #include <raylib.h>
 
 
-#define PADDLE_SPEED 250
+#define PADDLE_SPEED 300
 #define PADDLE_HEIGHT 60
 #define PADDLE_WIDTH 15
 
 
-typedef struct paddle_t paddle_t;
+typedef struct paddle_t {
+    Rectangle rec;
+    float dY;
+} paddle_t;
 
 
 paddle_t *paddle_init(Vector2 initial_pos);
 void paddle_render(paddle_t *paddle);
+void paddle_update(paddle_t *paddle);
 void paddle_delete(paddle_t *paddle);
 
 #endif
--- code/src/paddle.c
+++ code/src/paddle.c
@@ -1,16 +1,10 @@
 #include "paddle.h"
+#include "defines.h"
 
 #include <stdlib.h>
 
 #include <raylib.h>
-
-
-typedef struct paddle_t {
-    Rectangle rec;
-    Vector2 direction;
-    float speed;
-} paddle_t;
-
+#include <raymath.h>
 
 paddle_t *paddle_init(Vector2 initial_pos) {
     paddle_t *paddle = (paddle_t*) MemAlloc(sizeof(paddle_t));
@@ -20,17 +14,19 @@ paddle_t *paddle_init(Vector2 initial_pos) {
         .x = initial_pos.x,
         .y = initial_pos.y,
     };
-    paddle->direction = (Vector2) {
-        .x = 0.f,
-        .y = 0.f,
-    };
-    paddle->speed = 0.f;
+    paddle->dY = 0.f;
 
     return paddle;
 }
 
 void paddle_render(paddle_t *paddle) {
     DrawRectangleRec(paddle->rec, RAYWHITE);
+
+    return;
+}
+
+void paddle_update(paddle_t *paddle) {
+    paddle->rec.y = Clamp(paddle->rec.y + (paddle->dY * GetFrameTime()), 0, GAME_HEIGHT - paddle->rec.height);
 
     return;
 }
